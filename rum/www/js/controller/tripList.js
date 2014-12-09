176e424ca08f8b59a502f7e6ce1f5c4686a434be
Matthias Liedtke <mliedtke@gmx.de>
2014-12-09 12:25:37 +0100

    basic layout for trip list

--- rum/www/js/controller/tripList.js
+++ rum/www/js/controller/tripList.js
@@ -13,19 +13,22 @@ app.controller("tripListController", ["$scope", "$http", "$state", function($sco
 var trips = {
 	'1': {
 		id: 1,
-		name: 'trip1',
+		name: 'Amerika - Rundreise',
+		noParticipants: 5,
 		dateStart: new Date(2014, 11, 30),
 		dateEnd: new Date(2014, 11, 31)
 	},
 	'2': {
 		id: 2,
 		name: 'trip2',
+		noParticipants: 3,
 		dateStart: new Date(2015, 3, 10),
 		dateEnd: new Date(2015, 3, 15)
 	},
 	'3': {
 		id: 3,
 		name: 'trip3',
+		noParticipants: 1,
 		dateStart: new Date(2013, 0, 1),
 		dateEnd: new Date(2013, 0, 14)
 	}
