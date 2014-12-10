0f7534f95c4e37b5e5a5831cb0d5fef863c73597
Matthias Liedtke <mliedtke@gmx.de>
2014-12-10 18:30:57 +0100

    addCity view redone; autocompletion not completely working

--- rum/www/js/controller/addCity.js
+++ rum/www/js/controller/addCity.js
@@ -1,14 +1,54 @@
-app.controller("addCityController", ["$scope", "$timeout", "$state", function($scope, $timeout, $state) {
-	$scope.title = "Stadt hinzufügen";
+app.controller("addCityController", ["$scope", "$timeout", "$state", "$http", function($scope, $timeout, $state, $http) {
 	
-	var geocoder = new google.maps.Geocoder();
+//	var geocoder = new google.maps.Geocoder();
+//	
+//	 var autocomplete = new google.maps.places.Autocomplete(
+//			 (document.getElementById('searchText')),
+//			{ types: ['(cities)'] });
 	
-	 var autocomplete = new google.maps.places.Autocomplete(
-			 (document.getElementById('searchText')),
-			{ types: ['(cities)'] });
+	this.autocompletionData;
+	this.cityData;
+	var me = this;
 	
-	// provide autocompletion
-	this.autocomplete = function(){
-		
+	// initialize autocompletion service
+	var service = new google.maps.places.AutocompleteService();
+	
+	this.selectCity = function(city){
+		console.log('select city');
+		me.cityData.name = city.description;
+		me.autocompletionData = [];
 	}
+	
+	this.autocomplete = function(){
+		// check whether input field is empty
+		if(me.cityData.name == ""){
+			me.autocompletionData = [];
+			return;
+		}
+		// ask google places
+		var input = {
+			input: me.cityData.name,
+			types: ['(cities)'],
+			language: 'de' // not working!?
+		};
+		service.getPlacePredictions(input, callback);
+		// the callback function
+		function callback(predictions, status){
+			$timeout(function(){
+				if (status != google.maps.places.PlacesServiceStatus.OK) {
+				    console.log(status);
+				    return;
+				}
+					// if everything is fine
+					me.autocompletionData = predictions; 
+				}
+			);
+		}
+	}
+	
+	this.addCity = function(){
+		console.log('add city');
+	}
+	
+	
 }]);
