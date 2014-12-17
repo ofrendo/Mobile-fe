6450f00cb443157d655b62af509cefe5fd9093ed
Matthias Liedtke <mliedtke@gmx.de>
2014-12-17 14:42:38 +0100

    editCity changed

--- rum/www/js/controller/editCity.js
+++ rum/www/js/controller/editCity.js
@@ -1,9 +1,15 @@
 app.controller("editCityController", 
-	["$scope", "$http", "$state", "$ionicPopup", "loginService", "globals", 
-	function($scope, $http, $state, $ionicPopup, loginService, globals) {
+	["$scope", "$http", "$state", "$ionicPopup", "loginService", "globals", "$stateParams", 
+	function($scope, $http, $state, $ionicPopup, loginService, globals, $stateParams) {
 	
+	this.title = '';
+		
 	console.log("---INIT editCityController----");
 	loginService.onInit(function() {
 		globals.checkTripID();
 	});
+	
+	this.getCityData = function(){
+		console.log('INIT getCityData with id = ' + $stateParams.city_id);
+	}
 }]);
