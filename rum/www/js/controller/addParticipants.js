d33d93b95dfcbb452f6037993384310ffd649850
Max Hofmann <max.hofmann@sap.com>
2014-12-17 11:34:53 +0100

    Kontakte werden angezeigt + angular-mocks

--- rum/www/js/controller/addParticipants.js
+++ rum/www/js/controller/addParticipants.js
@@ -2,30 +2,42 @@ app.controller("addParticipantsController",
 		["$scope", "restAPI", "$timeout", "$stateParams",
         function($scope, restAPI, $timeout, $stateParams){
 	
-		try {
+	this.getContactList = function(){
 			console.log("Kontaktliste wird geladen");
+			function onSuccess(contacts) {
+			    console.log('Found ' + contacts.length + ' contacts.');
+			    console.log(contacts);
+			    //modify contacts to display photos
+			    for ( var i = 0; i < contacts.length; i++) {
+					if (contacts[i].photos != null) {
+						var imageURI = contacts[i].photos[0].value;
+						if (imageURI.substring(0,21)=="content://com.android") {
+							  photo_split=imageURI.split("%3A");
+							  imageURI="content://media/external/images/media/"+photo_split[1];
+							  contacts[i].photos[0].value = imageURI;
+							}
+					}
+				}
+				$scope.contacts = contacts;
+			};
+
+			function onError(contactError) {
+			    consolse.log('onError!');
+			};
+
+			// find all contacts with 'Bob' in any name field
+			var options      = new ContactFindOptions();
+			options.filter   = "";
+			options.multiple = true;
+			var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+			navigator.contacts.find(fields, onSuccess, onError, options);
+			
+			
 			document.addEventListener("deviceready", onDeviceReady, false);
 			function onDeviceReady() {
 				console.log("Kontakte werden geladen:");
 			    console.log(navigator.contacts);
 			};
-		} catch (e) {
-			console.log("Debug" + e);
-		}		
-			
-	this.getContactList = function(){
-		$timeout(function(){
-			console.log('INIT getParticipants with id = ' + $stateParams.trip_id);
-			restAPI.trip.readUsers($stateParams.trip_id, 
-				function(contacts){
-				$timeout(function(){
-					console.log('GET participants callback with data for ContactList:');
-					console.log(contacts);
-					$scope.contacts = contacts;
-				});
-				}
-			);
-		});
 
 	};
 	
