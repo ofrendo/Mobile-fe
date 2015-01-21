66a2539ed88623abfab7cbb344e81859519914db
Matthias Liedtke <mliedtke@gmx.de>
2015-01-21 13:18:04 +0100

    settings: change account settings implemented; change password formular added

--- rum/www/js/controller/settings.js
+++ rum/www/js/controller/settings.js
@@ -1,9 +1,10 @@
 app.controller("settingsController", 
-	["$scope",  "$translate", "$timeout",
-	function($scope,  $translate, $timeout) {
+	["$scope",  "$translate", "$timeout", "restAPI", "globals", "loginService",
+	function($scope,  $translate, $timeout, restAPI, globals, loginService) {
 		
 		//VARIABLES
 		var me = this;
+		$scope.pw = {};
 		this.languages = 
 			[
 			 {
@@ -16,14 +17,21 @@ app.controller("settingsController",
 			 }
 			];
 		
+		//INIT
+		console.log("----INIT settingsController----");
+		loginService.onInit(function(){
+			globals.setTripID(-1);
+			getAccountData();
+		});
+		
 		//FUNCTIONS
 		//changes the language
 		this.changeLanguage = function(selected_language){
 			console.log('Change to language: ' + selected_language);
 			$translate.use(selected_language).then(function(){
-				toast.showShort("Hallo")
+				console.log('-> successfully changed.');
 			});
-		}
+		};
 		
 		//sets the active language
 		this.initLanguage = function(){
@@ -31,6 +39,29 @@ app.controller("settingsController",
 				console.log('Active Language on load: ' + $translate.use())
 				// set active language
 				$scope.language = $translate.use();
+			});
+		};
+		
+		// save user data
+		this.saveUser = function() {
+			console.log('save user: ');
+			console.log($scope.user);
+			restAPI.user.update($scope.user.user_id, {user: $scope.user}, function(){
+				console.log('user data successfully saved');
+			});
+		};
+		
+		// save password
+		this.savePassword = function(){
+			console.log('save password:');
+			console.log($scope.pw);
+		};
+		
+		// get the user data from backend
+		function getAccountData(){
+			restAPI.user.read(globals.user.user_id, function(user){
+				console.log(user);
+				$scope.user = user;
 			});
 		}
 }]);
